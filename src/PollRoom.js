42ac4499ca03518831c8e3c7bcf32e3f604920c5
Eemeli Mark <eemeli.mark@vertex.fi>
2024-07-26 11:32:07 +0300

    a

--- src/PollRoom.js
+++ src/PollRoom.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { initializeApp } from "firebase/app";
-import { getFirestore, collection, setDoc, getDoc, doc, serverTimestamp } from "firebase/firestore";
+import { getFirestore, setDoc, getDoc, doc, serverTimestamp } from "firebase/firestore";
 import { customAlphabet } from "nanoid";
 const nanoid = customAlphabet("1234567890abcdefghkmnopqrstuvwxyz", 36);
 
